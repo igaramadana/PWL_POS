23c8bc44042bb898ceaf14f38b11015c298df366
igaramadana <igrmdns085@gmail.com>
2025-03-08 17:22:40 +0700

    Jobsheet 3 - Praktikum 2.2 (Relation Migration)

--- database/migrations/2025_03_08_015246_create_t_penjualan_table.php
+++ database/migrations/2025_03_08_015246_create_t_penjualan_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id('penjualan_id');
             $table->unsignedBigInteger('user_id')->index();
             $table->string('pembeli', 50);
-            $table->string('penjualan_kode', 20)->unique();
+            $table->string('penjualan_kode', 20);
             $table->dateTime('penjualan_tanggal');
             $table->timestamps();
 
