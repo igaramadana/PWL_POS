6e2db19d58b914732f41a68bbcb14db74eb7a307
igaramadana <igrmdns085@gmail.com>
2025-03-11 21:56:52 +0700

    Jobsheet 3 - Praktikum 6 - Eloquent ORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // Tambah data user dengan Eloquent Model
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+
+        // UserModel::insert($data); // Tambahkan data ke table m_user
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+
+        // Coba akses model UserModel
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 
@@ -22,3 +23,5 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 
 Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
