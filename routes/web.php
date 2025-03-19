cb1b69a8a06a83bb8effe3b471ae8ebddb7bc419
igaramadana <igrmdns085@gmail.com>
2025-03-19 22:15:22 +0700

    Jobsheet 5 - Praktikum 2 (Penerapan Layouting)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 
@@ -16,9 +17,7 @@ use App\Http\Controllers\LevelController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
 
