35c0e606d277762131ab55ecb141382c1eed3166
igaramadana <igrmdns085@gmail.com>
2025-03-20 19:45:19 +0700

    Jobsheet 6 - Praktikum 2 (Modal Ajax Edit Data User)

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,9 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']);
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
+    // Ajax Update
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
